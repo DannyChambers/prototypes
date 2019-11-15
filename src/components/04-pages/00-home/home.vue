5e90bc995cdbb4c2cbe89e4d740293198b35f430
Danny Clayden Chambers <dannyclaydenchambers@Dannys-MacBook-Pro-2.local>
2019-11-15 11:00:27 +0000

    Added Vue Router

--- src/components/04-pages/00-home/home.vue
+++ src/components/04-pages/00-home/home.vue
@@ -2,14 +2,18 @@
 <template>
   <div>
 
-
+    <H1 text="Home baby!" />
 
   </div>
 </template>
 
 <script>
+  import H1 from '../../00-atoms/02-typography/h1.vue'
   export default {
     name: 'Home',
+    components: {
+      H1
+    },
     props: {
       variant: String
     },
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+//Routes
+import Home from './components/04-pages/00-home/home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{path: '/', component: Home}
+]
+
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+});
 
 Vue.config.productionTip = false
 
-new Vue({
+new Vue({	
+  router,
   render: h => h(App),
 }).$mount('#app')
